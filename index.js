425d27ba5d1bb308bb71f48294e05709ced33818
suraffy <suraffy@gmail.com>
2024-03-16 09:21:48 +0300

    Export getProfile functionality

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const getVerifiedPaymentDetails = require("./lib/verify");
 const getQRCodeUrl = require("./lib/qrCode");
+const getProfile = require('./lib/profile')
 
 module.exports = getVerifiedPaymentDetails;
 module.exports = getQRCodeUrl;
+module.exports = getProfile;
