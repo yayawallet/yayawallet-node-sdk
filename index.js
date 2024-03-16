db880ad1c21ed4aea9ad935131e40082a80e1351
Ermiyas <ermiyasbitew239@gmail.com>
2024-03-16 09:50:30 +0300

    added transaction, transfer, invitation and user endpoints

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 const getVerifiedPaymentDetails = require("./lib/verify");
-const getQRCodeUrl = require("./lib/qrCode");
 
 module.exports = getVerifiedPaymentDetails;
-module.exports = getQRCodeUrl;
