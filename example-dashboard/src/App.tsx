44fdeeecf6155b5fd0b17a56f846a94c68fbd837
suraffy <suraffy@gmail.com>
2024-03-20 10:49:07 +0300

    File and Folder structure

--- example-dashboard/src/App.tsx
+++ example-dashboard/src/App.tsx
@@ -1,9 +1,11 @@
 const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Example Dashboard</h1>
+      <h1 className="text-3xl font-bold underline text-center">
+        Example Dashboard
+      </h1>
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- example-dashboard/tailwind.config.js
+++ example-dashboard/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
+    container: {
+      padding: '15px',
+      center: true
+    }
   },
   plugins: []
-};
+}
