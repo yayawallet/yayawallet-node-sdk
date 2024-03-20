a8182c26ebbaa9715397ff3c5ec60b92457d0f6c
suraffy <suraffy@gmail.com>
2024-03-20 13:37:39 +0300

    Update layout

--- example-dashboard/src/components/common/Sidebar.tsx
+++ example-dashboard/src/components/common/Sidebar.tsx
@@ -27,81 +27,83 @@ const Sidebar = () => {
         className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
         aria-label="Sidebar"
       >
-        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 ">
-          <a href="/" className="flex items-center ps-2.5 mb-5">
-            <img
-              src={yayawalletLogo}
-              className="h-10 sm:h-12"
-              alt="YaYaWallet Logo"
-            />
+        <div className="h-full  py-3 overflow-y-auto bg-gray-50">
+          <a href="/" className="flex items-center ps-2.5 mb-5 shadow-sm">
+            <img src={yayawalletLogo} className="h-12" alt="YaYaWallet Logo" />
           </a>
-          <ul className="space-y-2 font-medium">
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-              >
-                <span className="flex-1 ms-3 whitespace-nowrap">Profile</span>
-                <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full">
-                  Level 1
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-              >
-                <span className="flex-1 ms-3 whitespace-nowrap">
-                  Transactions
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-              >
-                <span className="flex-1 ms-3 whitespace-nowrap">Transfer</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-              >
-                <span className="flex-1 ms-3 whitespace-nowrap">Saving</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-              >
-                <span className="flex-1 ms-3 whitespace-nowrap">
-                  Institution
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-              >
-                <span className="flex-1 ms-3 whitespace-nowrap">
-                  Invitation
-                </span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
-              >
-                <span className="flex-1 ms-3 whitespace-nowrap">Sign out</span>
-              </a>
-            </li>
-          </ul>
+          <div className="px-3">
+            <ul className="space-y-2 font-medium">
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                >
+                  <span className="flex-1 ms-3 whitespace-nowrap">Profile</span>
+                  <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full">
+                    Level 1
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                >
+                  <span className="flex-1 ms-3 whitespace-nowrap">
+                    Transactions
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                >
+                  <span className="flex-1 ms-3 whitespace-nowrap">
+                    Transfer
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                >
+                  <span className="flex-1 ms-3 whitespace-nowrap">Saving</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                >
+                  <span className="flex-1 ms-3 whitespace-nowrap">
+                    Institution
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                >
+                  <span className="flex-1 ms-3 whitespace-nowrap">
+                    Invitation
+                  </span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+                >
+                  <span className="flex-1 ms-3 whitespace-nowrap">
+                    Sign out
+                  </span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </aside>
     </>
