a585d5e22ea1725eb59c94d47745352fe7bd4850
suraffy <suraffy@gmail.com>
2024-03-20 12:00:56 +0300

    Build a Sidebar component

--- example-dashboard/src/components/common/Sidebar.tsx
+++ example-dashboard/src/components/common/Sidebar.tsx
@@ -1,5 +1,111 @@
+import yayawalletLogo from '../../assets/yayawallet-brand.svg'
+
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  return (
+    <>
+      <button
+        data-drawer-target="logo-sidebar"
+        data-drawer-toggle="logo-sidebar"
+        aria-controls="logo-sidebar"
+        type="button"
+        className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+      >
+        <span className="sr-only">Open sidebar</span>
+        <svg
+          className="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+        </svg>
+      </button>
+
+      <aside
+        id="logo-sidebar"
+        className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 ">
+          <a href="/" className="flex items-center ps-2.5 mb-5">
+            <img
+              src={yayawalletLogo}
+              className="h-10 sm:h-12"
+              alt="YaYaWallet Logo"
+            />
+          </a>
+          <ul className="space-y-2 font-medium">
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+              >
+                <span className="flex-1 ms-3 whitespace-nowrap">Profile</span>
+                <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full">
+                  Level 1
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+              >
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  Transactions
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+              >
+                <span className="flex-1 ms-3 whitespace-nowrap">Transfer</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+              >
+                <span className="flex-1 ms-3 whitespace-nowrap">Saving</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+              >
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  Institution
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+              >
+                <span className="flex-1 ms-3 whitespace-nowrap">
+                  Invitation
+                </span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+              >
+                <span className="flex-1 ms-3 whitespace-nowrap">Sign out</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+    </>
+  )
 }
 
 export default Sidebar
