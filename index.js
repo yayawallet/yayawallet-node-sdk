91be824dc7845f83fd9ff2df35bead4eecdd511e
suraffy <suraffy@gmail.com>
2024-03-22 10:41:10 +0300

    Prettier: code style formatting

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const airtime = require("./lib/airtime")
-const generateSignature = require("./lib/generateSignature.js")
-const institution = require("./lib/institution")
-const invitation = require("./lib/invitation")
-const saving = require("./lib/saving")
-const time = require("./lib/time")
-const transaction = require("./lib/transaction")
-const transfer = require("./lib/transfer")
-const user = require("./lib/user")
-const verify = require("./lib/verify")
+const airtime = require('./lib/airtime');
+const generateSignature = require('./lib/generateSignature.js');
+const institution = require('./lib/institution');
+const invitation = require('./lib/invitation');
+const saving = require('./lib/saving');
+const time = require('./lib/time');
+const transaction = require('./lib/transaction');
+const transfer = require('./lib/transfer');
+const user = require('./lib/user');
+const verify = require('./lib/verify');
 
-module.exports = {...airtime, ...generateSignature, ...institution, ...invitation, ...saving, ...time, ...transaction, ...transfer, ...user, ...verify};
+module.exports = {
+  ...airtime,
+  ...generateSignature,
+  ...institution,
+  ...invitation,
+  ...saving,
+  ...time,
+  ...transaction,
+  ...transfer,
+  ...user,
+  ...verify
+};
